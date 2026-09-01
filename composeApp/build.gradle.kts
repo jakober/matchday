0cc1bfeccec58eb53fcd9e9aa2a496bd5c4fd1b7
jakober <jakober@bairle.de>
2026-09-01 15:29:39 +0200

    Android-Versionsnummer mitfuehren

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -94,8 +94,8 @@ android {
         applicationId = "com.jakober.matchday"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 18
+        versionName = "0.18"
     }
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
